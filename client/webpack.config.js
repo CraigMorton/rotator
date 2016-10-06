e8b19e8e311439e6d9abed44899ac28c009223a5
Valerie Gibson <vgibson14@gmail.com>
2016-10-06 14:02:28 +0100

    adds postcss

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,20 @@
+var autoprefixer = require('autoprefixer');
+
 config = {
   entry: "./app.js",
   output: {
     filename: "bundle.js",
     path: "build"
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  module: { //NEW
+    loaders: [
+      { test: /\.css$/,loader: 'style-loader!css-loader!postcss-loader'}
+    ]
+  },
+  postcss: function () { //NEW
+    return [autoprefixer];
+  }
 }
 
 module.exports = config;
