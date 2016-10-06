b4f2a0336a4ff72d01ccfb42340a7bef0fe37080
Valerie Gibson <vgibson14@gmail.com>
2016-10-06 09:19:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var students = [
+"Katrina",
+"Simon",
+"John",
+"Gillian",
+"Michael",
+"Stuart",
+"Charley",
+"Robbie",
+"Alan",
+"DaMo",
+"Emma",
+"Sian",
+"Phil",
+"David_Mc",
+"Hannah",
+"Lee",
+"James_H",
+"Kiyohito",
+"James_S"
+];
+
+console.log(students);
+
