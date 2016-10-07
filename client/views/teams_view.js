9fc8305056ed02887798c687221ca92b799dea7c
JarrodBennie <jarrodbennie@icloud.com>
2016-10-07 08:52:50 +0100

    changed prototype syntax

--- client/views/teams_view.js
+++ client/views/teams_view.js
@@ -2,27 +2,29 @@ var TeamsView = function(teams) {
   this.teams = teams;
 }
 
-TeamsView.prototype.createImage = function(student) {
-  var newImage = document.createElement('img');
-  newImage.src = '/images/' + student + '.jpg';
-  return newImage;
-}
+TeamsView.prototype = {
+  createImage: function(student) {
+    var newImage = document.createElement('img');
+    newImage.src = '/images/' + student + '.jpg';
+    return newImage;
+  },
 
-TeamsView.prototype.displayTeam = function(team) {
-  var container = document.querySelector('#teams');
-  var teamContainer = document.createElement('div');
+  displayTeam: function(team) {
+    var container = document.querySelector('#teams');
+    var teamContainer = document.createElement('div');
 
-  for (var student of team) {
-    var newImage = this.createImage(student);
-    teamContainer.appendChild(newImage);
-  }
+    for (var student of team) {
+      var newImage = this.createImage(student);
+      teamContainer.appendChild(newImage);
+    }
 
-  container.appendChild(teamContainer);
-}
+    container.appendChild(teamContainer);
+  },
 
-TeamsView.prototype.render = function() {
-  for (var team of this.teams) {
-    this.displayTeam(team);
+  render: function() {
+    for (var team of this.teams) {
+      this.displayTeam(team);
+    }
   }
 }
 
